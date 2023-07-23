c38be6ec5f6823e40635b12084e2a4b2bcd6b8b8
RJURU <110449011+RJURU@users.noreply.github.com>
2023-07-23 07:14:56 +0100

    Fix Data Upload For Mobile

--- src/Components/FileInput.jsx
+++ src/Components/FileInput.jsx
@@ -31,7 +31,6 @@ const FileInput = ({ setSelectedFileData }) => {
             file:text-sm file:font-semibold
             file:text-white hover:file:bg-blue-500"
             type="file"
-            accept=".json,application/json"
             onChange={onChange}
         ></input>
     );
